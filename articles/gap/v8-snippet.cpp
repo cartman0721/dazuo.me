2d1af2705fef0ac28d73b13a9ce7fb76d32209d7
cartman0721 <sundazuo@gmail.com>
2012-03-24 15:38:50 +0800

    delete articles

--- articles/gap/v8-snippet.cpp
+++
@@ -1,4 +0,0 @@
-#define TYPE_CHECK(T, S)                                     \
-    while (false) {                                              \
-      *(static_cast<T* volatile*>(0)) = static_cast<S*>(0);      \
-    }
